025fdee76ca8a0b057aae3566d7b48e518c3f799
Avinash <avinashchowdhury81@gmail.com>
2024-07-22 00:57:06 +0530

    half backend2

---
+++ Server/middleware.js
@@ -0,0 +1,26 @@
+const {JWT_SECRET} = require('./config');
+const jwt = require('jsonwebtoken');
+
+const authmiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+
+    const token  = authheader.split('')[1];
+
+    try{
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        req.userId = decoded.userId;
+
+        next()
+    } catch(err){
+        return res.status(403).json({ error: 'Invalid token' });
+    }
+}
+
+module.exports ={
+    authmiddleware
+};
