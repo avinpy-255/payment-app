e8cbda6b10f69af82105a525b97ddb5887cc5809
Avinash <avinashchowdhury81@gmail.com>
2024-08-02 15:03:30 +0530

    updated vercel.json

--- Server/index.js
+++ Server/index.js
@@ -1,34 +1,24 @@
 const express = require("express");
-const mainRouter = require("./Routes/index")
-const {connectDB} = require('./database')
+const mainRouter = require("./Routes/index");
+const { connectDB } = require("./database");
 const cors = require("cors");
 const { PORT } = require("./config");
 const app = express();
 
+app.use(
+  cors()
+);
+app.use(express.json());
 
-const corsOptions = {
-    origin: 'https://payment-app-frontend-six.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  };
-
-app.use(cors(corsOptions));
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.json({
-        message: "Hello World"
-    })
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
 });
 
-app.use('/api/v1', mainRouter);
+app.use("/api/v1", mainRouter);
 
-app.listen(PORT, ()=>{
-    connectDB();
-    console.log(`Server is running on port ${PORT}`);
-})
-
-
-
-
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
