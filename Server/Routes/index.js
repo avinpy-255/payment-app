6de529a8df68fa6188d545109bcdbbced5d59ad3
Avinash <avinashchowdhury81@gmail.com>
2024-08-02 15:58:10 +0530

    fresh deploy 4

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const userRouter = require("./user")
 const accountRouter = require("./account")
 const router = express.Router();
+const cors = require("cors")
+const app = express();
+
+app.use(
+    cors(
+  {    origin: ["https://payment-app-frontend-six.vercel.app"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true, // enable cookies in requests
+  }
+    )
+  );
+  app.use(express.json());
+  
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Hello World2",
+    });
+  });
 
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
