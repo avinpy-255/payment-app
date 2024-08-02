de3077371328c7449fc0a90090334f84aead2199
Avinash <avinashchowdhury81@gmail.com>
2024-08-02 16:22:07 +0530

    freshdeploy

--- Server/index.js
+++ Server/index.js
@@ -5,17 +5,12 @@ const cors = require("cors");
 const { PORT } = require("./config");
 const app = express();
 
-// CORS configuration
-const corsOptions = {
-    origin: ["https://payment-app-frontend-six.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-  };
-  
-  // Apply CORS middleware
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use (cors( {
+  origin: "https://payment-app-frontend-git-main-avinpy-255s-projects.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}))
 
 app.get("/", (req, res) => {
   res.json({
