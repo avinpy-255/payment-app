24b9a806b7cd6603a23820a5e74271c49a9fd92e
Avinash <avinashchowdhury81@gmail.com>
2024-08-02 15:17:42 +0530

    updated index.js  3

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mainRouter = require("./Routes/index");
 const { connectDB } = require("./database");
+const cors = require("cors");
 const { PORT } = require("./config");
 const app = express();
 
-
+app.use(
+  cors()
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
