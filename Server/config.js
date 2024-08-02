90058100ce6325347eefd20e4ba31ce951e74804
Avinash <avinashchowdhury81@gmail.com>
2024-08-02 15:43:03 +0530

    fresh deploy 2

--- Server/config.js
+++ Server/config.js
@@ -3,12 +3,10 @@ require('dotenv').config();
 const JWT_SECRET = process.env.JWT_SECRET;
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
-const ORIGIN = process.env.ORIGIN;
 
 module.exports = {
     JWT_SECRET,
     MONGO_URI,
-    PORT,
-    ORIGIN
+    PORT
 };
 
